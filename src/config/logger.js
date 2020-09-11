446d0c35537eacb2fec1aeb44c0c13fec9e423a1
Aleksander Chromik <achromik@gmail.com>
2020-09-11 16:33:05 +0200

    Add logger

--- src/config/logger.js
+++ src/config/logger.js
@@ -1,4 +1,36 @@
 const bunyan = require('bunyan');
-const log = bunyan.createLogger({ name: 'ec2-app' });
 
-module.exports = { log };
+exports.log = bunyan.createLogger({
+  name: 'ec2-app',
+  serializers: {
+    err: bunyan.stdSerializers.err,
+    req: require('bunyan-express-serializer'),
+    res: bunyan.stdSerializers.res,
+  },
+});
+
+exports.logResponse = function (id, body, statusCode) {
+  const log = this.log.child(
+    {
+      id: id,
+      statusCode: statusCode,
+      body: body,
+    },
+    true
+  );
+  log.info('response');
+};
+
+exports.logRequest = function (req) {
+  const log = this.log.child(
+    {
+      id: req.id,
+      method: req.method,
+      path: req.path,
+      body: req.body,
+      headers: req.headers,
+    },
+    true
+  );
+  log.info('request');
+};
--- src/index.js
+++ src/index.js
@@ -1,11 +1,68 @@
 const express = require('express');
 const app = express();
+const addRequestId = require('express-request-id')();
 
-const { log } = require('./config/logger');
+const AppLogger = require('./config/logger');
 const { connectDB } = require('./config/db');
 
 const port = process.env.PORT || 3000;
 
 connectDB();
 
-app.listen(port, () => log.info(`Listening on port ${port}!`));
+app.use(express.json());
+app.use(express.urlencoded());
+app.use(addRequestId);
+
+app.use((req, res, next) => {
+  AppLogger.logRequest(req);
+
+  next();
+});
+
+app.use((req, res, next) => {
+  const afterResponse = () => {
+    res.removeListener('finish', afterResponse);
+    res.removeListener('close', afterResponse);
+    const log = AppLogger.log.child(
+      {
+        id: req.id,
+      },
+      true
+    );
+    log.info({ res }, 'response');
+  };
+
+  res.on('finish', afterResponse);
+  res.on('close', afterResponse);
+
+  next();
+});
+
+app.get('/status', (req, res, next) => {
+  res.send('Welcome');
+});
+
+app.post('/status', (req, res, next) => {
+  res.send({ data: req.body });
+});
+
+app.use((req, res, next) => {
+  const response = { status: 404, error: 'Not found' };
+  AppLogger.log.warn({ id: req.id, path: req.path }, 'Not Found');
+
+  res.status(404).send(response);
+});
+
+app.use((error, req, res, next) => {
+  AppLogger.log.error({ err: error, req });
+
+  res.status(error.status || 500).send({
+    error: {
+      status: error.status || 500,
+      message: error.message || 'Internal Server Error',
+    },
+  });
+  next();
+});
+
+app.listen(port, () => AppLogger.log.info(`Listening on port ${port}!`));
