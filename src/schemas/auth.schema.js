b23aff3c3564d4d52299c54c825050b8bee96102
Aleksander Chromik <achromik@gmail.com>
2020-09-13 12:09:47 +0200

    Update paylaoad signup schema

--- src/schemas/auth.schema.js
+++ src/schemas/auth.schema.js
@@ -1,5 +1,7 @@
 const Joi = require('joi');
 
+const { userRole } = require('../config/constants');
+
 const signupSchema = Joi.object({
   username: Joi.string().min(5).required(),
   email: Joi.string().email().required(),
@@ -8,7 +10,7 @@ const signupSchema = Joi.object({
     .min(1)
     .max(3)
     .unique()
-    .items(Joi.string().valid('user', 'moderator', 'admin')),
+    .items(Joi.string().valid(...Object.values(userRole))),
 });
 
 module.exports = { signupSchema };
