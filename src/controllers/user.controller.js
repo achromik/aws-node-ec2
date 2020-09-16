675fe813437a79960311151b77c5cfc20390cc15
Aleksander Chromik <achromik@gmail.com>
2020-09-16 10:37:08 +0200

    Add routes to test JWT authorization

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,20 +1,15 @@
-const AppLogger = require('../config/logger');
-const db = require('../models');
-
-const User = db.user;
-
-const saveUser = async (req, res, next) => {
-  const { body } = req;
-  try {
-    const user = new User(body);
-    const result = await user.save({ body });
-    res.send({ data: result });
-  } catch (err) {
-    AppLogger.log.error({ err }, 'Saving user failed');
-    res
-      .status(400)
-      .send({ error: { statusCode: 400, message: 'Saving user failed' } });
-  }
+exports.allAccess = (req, res) => {
+  res.status(200).send('Public Content');
 };
 
-module.exports = { saveUser };
+exports.userBoard = (req, res) => {
+  res.status(200).send('User Content');
+};
+
+exports.adminBoard = (req, res) => {
+  res.status(200).send('Admin Content');
+};
+
+exports.moderatorBoard = (req, res) => {
+  res.status(200).send('Moderator Content');
+};
--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,5 +1,26 @@
+const express = require('express');
+
+const { authJwt } = require('../middlewares');
 const controller = require('../controllers/user.controller');
 
-module.exports = (app) => {
-  app.post('/user', controller.saveUser);
+module.exports = (app, prefix = '', path = '/test') => {
+  const router = express.Router();
+
+  router.get('/all', controller.allAccess);
+
+  router.get('/user', [authJwt.verifyToken], controller.userBoard);
+
+  router.get(
+    '/mod',
+    [authJwt.verifyToken, authJwt.isModerator],
+    controller.moderatorBoard
+  );
+
+  router.get(
+    '/admin',
+    [authJwt.verifyToken, authJwt.isAdmin],
+    controller.adminBoard
+  );
+
+  app.use(prefix + path, router);
 };
