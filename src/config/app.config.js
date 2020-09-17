8e1fbec44da9a142959952760c9d13e13ee74020
Aleksander Chromik <achromik@gmail.com>
2020-09-17 12:04:26 +0200

    Add gracefull exiting

--- src/config/app.config.js
+++ src/config/app.config.js
@@ -1,3 +1,4 @@
 module.exports = {
+  port: process.env.POST || 3000,
   baseApiPath: process.env.API_PATH_PREFIX || '',
 };
--- src/server.js
+++ src/server.js
@@ -1,15 +1,43 @@
+const mongoose = require('mongoose');
 const app = require('./app');
 const AppLogger = require('./config/logger');
 const { connectDB } = require('./config/db');
 const seedRoles = require('./config/seedRoles');
 const { userRole } = require('./config/constants');
+const { port } = require('./config/app.config');
 
-const port = process.env.PORT || 3000;
+let server;
 
-connectDB().then(() => {
-  seedRoles(Object.values(userRole));
+async function start() {
+  try {
+    AppLogger.log.info('Starting server...');
+    await connectDB();
 
-  app.listen(port, () => {
-    AppLogger.log.info(`Listening on port ${port}!`);
+    seedRoles(Object.values(userRole));
+
+    server = app.listen(port, () => {
+      AppLogger.log.info(`Listening on port ${port}...`);
+    });
+  } catch (err) {
+    AppLogger.log.error('Unable to start server!', err.message);
+
+    process.exit(1);
+  }
+}
+
+start();
+
+process.on('SIGTERM', () => {
+  AppLogger.log.info('SIGTERM signal received.');
+
+  AppLogger.log.info('Closing http server...');
+  server.close(() => {
+    AppLogger.log.info('Http server closed.');
+
+    mongoose.connection.close(false, () => {
+      AppLogger.log.info('MongoDb connection closed.');
+
+      process.exit(0);
+    });
   });
 });
