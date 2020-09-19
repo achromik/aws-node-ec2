6e0581ac37a736f65a84fa603fce1f7dc17fdd1c
Aleksander Chromik <achromik@gmail.com>
2020-09-19 12:42:45 +0200

    Create routes in App component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,45 @@
 import React from 'react';
+import { Switch, Route, Link } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
-function App() {
+import './App.css';
+
+import { Home } from './components/Home';
+import { Login } from './components/Login';
+import { Register } from './components/Register';
+import { Profile } from './components/Profile';
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <nav className="navbar navbar-expand navbar-dark bg-dark">
+        <div className="navbar-nav ml-auto">
+          <li className="nav-item">
+            <Link to={'/login'} className="nav-link">
+              Login
+            </Link>
+          </li>
+
+          <li className="nav-item">
+            <Link to={'/register'} className="nav-link">
+              Sign Up
+            </Link>
+          </li>
+        </div>
+      </nav>
+      <div className="container mt-3">
+        <Switch>
+          <Route exact path={['/', '/home']} component={Home} />
+          <Route exact path="/login" component={Login} />
+          <Route exact path="/register" component={Register} />
+          <Route exact path="/profile" component={Profile} />
+          {/* <Route path="/user" component={BoardUser} />
+          <Route path="/mod" component={BoardModerator} />
+          <Route path="/admin" component={BoardAdmin} /> */}{' '}
+          */}
+        </Switch>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
